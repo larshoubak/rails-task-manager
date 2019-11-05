f33047160716d53f54833cb0a88392744619ac34
Lars Houbak <lars.houbak@gmail.com>
2019-11-05 22:51:25 +0100

    task 2 day 27

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Read all
+  get '/tasks', to: 'tasks#index'
+
+  # Create task
+  get '/tasks/new', to: 'tasks#new' # display form, trigger submit
+  post '/tasks', to: 'tasks#create' # handle POST request
+
+  # Edit/update task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit # form already with data
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Read/show one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # Delete one task
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
+
+# add two routes to handle the creation of a task.
+# One route is there to display the Task form,
+# and another one is there to handle the POST request generated when submitting this form. Try to use directly the form_for helper in your view.
